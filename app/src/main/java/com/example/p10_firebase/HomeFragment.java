018cb03140ce7fd4b8eada7d486bdee552fd3176
Pol <pbermejo@elpuig.xeill.net>
2022-04-05 16:54:38 +0200

    hora en los post ordenada desc

--- app/src/main/java/com/example/p10_firebase/HomeFragment.java
+++ app/src/main/java/com/example/p10_firebase/HomeFragment.java
@@ -54,7 +54,7 @@ public class HomeFragment extends Fragment {
 
         RecyclerView postsRecyclerView = view.findViewById(R.id.postsRecyclerView);
 
-        Query query = FirebaseFirestore.getInstance().collection("posts").limit(50);
+        Query query = FirebaseFirestore.getInstance().collection("posts").limit(50).orderBy("timestamp", Query.Direction.DESCENDING);
 
         FirestoreRecyclerOptions<Post> options = new FirestoreRecyclerOptions.Builder<Post>()
                 .setQuery(query, Post.class)
